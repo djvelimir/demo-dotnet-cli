f2641ce912705c64c7c04e06f4a841eca1e545fa
Velimir Đurković <velimir.djurkovic@gmail.com>
2022-10-26 20:30:15 +0200

    Renamed field, updated diagram

--- Shuffler/StringShuffler.cs
+++ Shuffler/StringShuffler.cs
@@ -2,7 +2,7 @@ namespace DemoDotnetCli.Shuffler
 {
     public class StringShuffler : IStringShuffler
     {
-        private Random rng = new Random();
+        private readonly Random random = new Random();
 
         public string Shuffle(string characters)
         {
@@ -12,7 +12,7 @@ namespace DemoDotnetCli.Shuffler
             while (n > 1)
             {
                 n--;
-                int k = rng.Next(n + 1);
+                int k = random.Next(n + 1);
                 string value = list[k];
                 list[k] = list[n];
                 list[n] = value;
